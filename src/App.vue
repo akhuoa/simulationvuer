f5624b0a25c4638b995d3865ac9327da4b21f1bd
Alan Garny <agarny@hellix.com>
2020-11-23 17:52:27 +1300

    Renamed the original HelloWorld.vue file to SimulationVuer.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SimulationVuer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SimulationVuer from './components/SimulationVuer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SimulationVuer
   }
 }
 </script>
---
+++ src/components/SimulationVuer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="simulation-container">
+    <h1>SimulationVuer</h1>
+    <p>
+      This is the SimulationVuer.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SimulationVuer'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.simulation-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
