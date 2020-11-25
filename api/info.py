93d3f6a72555a247e69e23e04312a4b435a7715f
Alan Garny <agarny@hellix.com>
2020-11-26 11:05:17 +1300

    Retrieve some model parameters.
    The ones that are needed to do stellate/vagus stimulation (see https://github.com/hsorby/docker_opencor_run_model/blob/master/run_model.py).

--- api/info.py
+++ api/info.py
@@ -9,12 +9,25 @@ res = {'valid': valid}
 
 if valid:
     data = simulation.data()
-    simulation_unit = simulation.results().voi().unit()
+    data_states = data.states()
+    data_constants = data.constants()
+    results = simulation.results()
+    results_states = results.states()
+    results_constants = results.constants()
 
     res.update({'simulation': {'starting_point': data.starting_point(),
                                'ending_point': data.ending_point(),
                                'point_interval': data.point_interval(),
-                               'unit': simulation_unit}})
+                               'unit': results.voi().unit()}})
+    res.update({'model': [{'name': 'Membrane/V_ode',
+                           'value': data_states['Membrane/V_ode'],
+                           'unit': results_states['Membrane/V_ode'].unit()},
+                          {'name': 'Rate_modulation_experiments/ACh',
+                           'value': data_constants['Rate_modulation_experiments/ACh'],
+                           'unit': results_constants['Rate_modulation_experiments/ACh'].unit()},
+                          {'name': 'Rate_modulation_experiments/Iso_1_uM',
+                           'value': data_constants['Rate_modulation_experiments/Iso_1_uM'],
+                           'unit': results_constants['Rate_modulation_experiments/Iso_1_uM'].unit()}]})
 else:
     res.update({'error': simulation.issues()})
 
