918f46f6d0cf69e37d3260ab1cbc23e722f5f40c
Alan Garny <agarny@hellix.com>
2021-08-12 15:26:14 +1200

    Can once again test our SimulationVuer locally.

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,12 @@
     <p>
       Instance showing the simulation of the <a href="https://models.physiomeproject.org/e/611/HumanSAN_Fabbri_Fantini_Wilders_Severi_2017.cellml/view">Fabbri <em>et al.</em> (2017)</a> model:
     </p>
-    <SimulationVuer :apiLocation="apiLocation" :resource="resource" />
+    <SimulationVuer :apiLocation="apiLocation" :entry="entry" />
     <hr />
     <p>
       Instance showing the simulation of the <a href="https://models.physiomeproject.org/e/611/HumanSAN_Fabbri_Fantini_Wilders_Severi_2017.cellml/view">Fabbri <em>et al.</em> (2017)</a> + <a href="https://models.physiomeproject.org/workspace/694">Gerstner & Kistler (2002)</a> composite model:
     </p>
-    <SimulationVuer :apiLocation="apiLocation" :resource="compositeResource" style="height: 500px;" />
+    <SimulationVuer :apiLocation="apiLocation" :entry="compositeEntry" style="height: 500px;" />
     <hr />
   </div>
 </template>
@@ -33,8 +33,18 @@ export default {
   data: function () {
     return {
       apiLocation: process.env.VUE_APP_API_LOCATION,
-      resource: "https://models.physiomeproject.org/workspace/486/rawfile/55879cbc485e2d4c41f3dc6d60424b849f94c4ee/HumanSAN_Fabbri_Fantini_Wilders_Severi_2017.cellml",
-      compositeResource: "https://models.physiomeproject.org/workspace/698/rawfile/f3fc911063ac72ed44e84c0c5af28df41c25d452/fabbri_et_al_based_composite_SAN_model.sedml",
+      entry: {
+        "dataset": "https://sparc.science/datasets/135?type=dataset",
+        "description": "CellML version of the Fabbri et al. 2017 mathematical model of the spontaneous electrical activity of a human sinoatrial node (SAN) pacemaker cell",
+        "name": "Computational analysis of the human sinus node action potential - Model development and effects of mutations",
+        "resource": "https://models.physiomeproject.org/workspace/486/rawfile/55879cbc485e2d4c41f3dc6d60424b849f94c4ee/HumanSAN_Fabbri_Fantini_Wilders_Severi_2017.cellml",
+      },
+      compositeEntry: {
+        "dataset": "https://sparc.science/datasets/157?type=dataset",
+        "description": "CellML version of the Fabbri et al. 2017 human sinoatrial node (SAN) cell model and an ODE-based version of the Gerstner & Kistler 2002 brain stem model combined to demonstrate the effect of brain stem activity on heart rate",
+        "name": "Fabbri-based composite SAN model",
+        "resource": "https://models.physiomeproject.org/workspace/698/rawfile/f3fc911063ac72ed44e84c0c5af28df41c25d452/fabbri_et_al_based_composite_SAN_model.sedml",
+      },
     };
   },
 };
