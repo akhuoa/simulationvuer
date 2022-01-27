c7f506a62f49375396c381523b1b008db8bda37e
Alan Garny <agarny@hellix.com>
2022-01-27 19:39:02 +1300

    Some minor cleaning up.

--- src/components/SimulationVuer.vue
+++ src/components/SimulationVuer.vue
@@ -500,18 +500,6 @@ export default {
 
             select.options = possibleValues;
 
-
-            let possibleValues = [];
-
-            input.possibleValues.forEach(value => {
-              possibleValues.push({
-                key: value.value,
-                label: value.name,
-                value: value.value,
-              });
-            });
-
-            select.options = possibleValues;
             this.$refs.input.appendChild(select.$el);
           } else {
             let slider = new VueSlider({
