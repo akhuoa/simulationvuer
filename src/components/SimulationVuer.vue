b318ef010807d52df4b787f2bd387e22c52c0853
Alan Garny <agarny@hellix.com>
2022-08-03 11:16:33 +1200

    Simulation UI information file for dataset 17.

--- src/components/SimulationVuer.vue
+++ src/components/SimulationVuer.vue
@@ -41,6 +41,7 @@ import { evaluateValue, evaluateSimulationValue } from "./common.js";
 import { validJson } from "./json.js";
 import { initialiseUi, finaliseUi } from "./ui.js";
 import simulationUiInfo4 from "./res/simulationUiInfo4.json";
+import simulationUiInfo17 from "./res/simulationUiInfo17.json";
 import simulationUiInfo135 from "./res/simulationUiInfo135.json";
 import simulationUiInfo157 from "./res/simulationUiInfo157.json";
 
@@ -251,6 +252,7 @@ export default {
       } else {
         let simulationUiInfos = {
           4: simulationUiInfo4,
+          17: simulationUiInfo17,
           135: simulationUiInfo135,
           157: simulationUiInfo157,
         };
