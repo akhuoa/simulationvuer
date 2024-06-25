71e1c839eb41e00ead90a68ee005a01db3e7e161
Alan Garny <agarny@hellix.com>
2024-06-25 17:15:17 +1200

    JavaScript formatting.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
-import { createApp } from 'vue'
-import * as VueRouter from 'vue-router'
-import App from './App.vue'
-import ElementPlus from 'element-plus'
+import { createApp } from "vue";
+import * as VueRouter from "vue-router";
+import App from "./App.vue";
+import ElementPlus from "element-plus";
 
-const routes = [
-  { path: '/'},
-]
-
+const routes = [{ path: "/" }];
 const router = VueRouter.createRouter({
   // Provide the history implementation to use. We are using the hash history for simplicity here.
   history: VueRouter.createWebHashHistory(),
   routes,
-})
+});
+const app = createApp(App);
 
-const app = createApp(App)
-app.use(router)
-app.use(ElementPlus)
-app.mount('#app')
+app.use(router);
+app.use(ElementPlus);
+app.mount("#app");
